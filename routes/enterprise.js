6340feba93cd2bb38f2a0487f88bdfcb4b2fa99b
Lauren Laskerr <llask@me.com>
2019-09-21 09:15:21 -0400

    routes

--- routes/enterprise.js
+++ routes/enterprise.js
@@ -3,9 +3,36 @@ const router  = express.Router();
 
 
 router.get('/signup', (req,res,next) => {
-
   res.render("enterprise-views/signup")
 })
+
+
+router.post('/signup', (req,res,next) => {
+  let name = req.body.name
+  let username = req.body.username
+  let password = req.body.password
+  let email = req.body.email
+  let company = req.body.company
+
+  const saltRounds = 10;
+
+
+  const salt  = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  User.create({
+    name: name,
+    username: username,
+    password: hash,
+    email: email,
+    company: company
+  }).then(data => {
+    res.redirect("/enterprise/login")
+
+  }).catch(err => next(err))
+})
+
+
 
 
 router.get('/login', (req,res,next) => {
