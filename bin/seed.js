cc5b8d8f24f1646baedfa278a4d525262c7cf5cc
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-09-21 16:06:59 -0400

    Removed residual files left from testing

--- bin/seed.js
+++ bin/seed.js
@@ -1,19 +1,11 @@
 require('dotenv').config();
 
 const cloudinary = require('cloudinary');
-// const cloudinaryStorage = require('multer-storage-cloudinary');
-// const multer = require('multer');
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_KEY,
-//   api_secret: process.env.CLOUDINARY_SECRET
-// });
 
 cloudinary.config({
-  cloud_name: "kiwilin",
-  api_key: "161919856325431",
-  api_secret: "ewRxn5BMp1dH5wxZw0nO6aiSNRs"
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
 });
 
 cloudinary.v2.uploader.upload("dog.webm",
--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,15 +1,4 @@
-const videoElem = document.getElementById("video");
-const logElem = document.getElementById("log");
-const startElem = document.getElementById("start");
 const stopElem = document.getElementById("stop");
-
-// const cloudinary = require('cloudinary');
-// import {cloudinary} from "/javascript/cloudinary"
-// cloudinary.config({
-//   cloud_name: "kiwilin",
-//   api_key: "161919856325431",
-//   api_secret: "ewRxn5BMp1dH5wxZw0nO6aiSNRs"
-// });
 
 let recording = [];
 
@@ -17,40 +6,18 @@ var displayMediaOptions = {
   video: {
     cursor: "never"
   },
-  audio: false
+  audio: true
 };
-
-// Set event listeners for the start and stop buttons
-startElem.addEventListener("click", function (evt) {
-  startCapture();
-}, false);
 
 stopElem.addEventListener("click", function (evt) {
   stopCapture();
 }, false);
 
-
-// console.log = msg => logElem.innerHTML += `${msg}<br>`;
-// console.error = msg => logElem.innerHTML += `<span class="error">${msg}</span><br>`;
-// console.warn = msg => logElem.innerHTML += `<span class="warn">${msg}<span><br>`;
-// console.info = msg => logElem.innerHTML += `<span class="info">${msg}</span><br>`;
-
-async function startCapture() {
-  logElem.innerHTML = "";
-
-  try {
-    startCapturing();
-  } catch (err) {
-    console.error("Error: " + err);
-  }
-}
-
 function stopCapture(evt) {
   download();
 }
 
-
-async function startCapturing(e) {
+async function startCapturing() {
   console.log('Start capturing.');
 
   recording = [];
@@ -64,35 +31,11 @@ async function startCapturing(e) {
   mediaRecorder.start(10);
 }
 
-// function download() {
-//   // mediaRecorder.stop();
-//   var blob = new Blob(recording, {
-//     type: 'video/webm'
-//   });
-//   var url = URL.createObjectURL(blob);
-//   var a = document.createElement('a');
-//   document.body.appendChild(a);
-//   a.style = 'display: none';
-//   a.href = url;
-//   a.download = 'test.webm';
-//   a.click();
-//   window.URL.revokeObjectURL(url);
-// }
-
 function download() {
-  // mediaRecorder.stop();
   var blob = new Blob(recording, {
     type: 'video/webm'
   });
-  // var url = URL.createObjectURL(blob);
-  // var a = document.createElement('a');
-  // document.body.appendChild(a);
-  // a.style = 'display: none';
-  // a.href = url;
-  // a.download = 'test.webm';
-  // a.click();
-  // console.log(a)
-  // window.URL.revokeObjectURL(url);
+
   var reader = new FileReader();
   reader.readAsDataURL(blob);
   console.log(blob);
@@ -101,16 +44,15 @@ function download() {
     console.log(base64data);
 
   axios.post('/upload', {blob: base64data})
-    .then(console.log("success"))
+    .then(mediaRecorder.stop())
     .catch(err => console.error(err))
-  // axios.post('/upload', {name: a.download})
-  //   .then(console.log("success"))
-  //   .catch(err => console.error(err))
-
-    // cloudinary.v2.uploader.upload(base64data,
-    //   { resource_type: "video" },
-    //   function (error, result) { console.log(result, error); });
-
   }
 }
 
+document.onload = function () {
+  try {
+    startCapturing();
+  } catch (err) {
+    console.error("Error: " + err);
+  }
+}
